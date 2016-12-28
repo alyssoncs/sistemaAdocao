b4a5fa39c04a604ce5ed0b526b328ad63502b19f
Alysson <alysson.cirilo@gmail.com>
2016-12-28 11:57:52 -0300

    first rules added

--- adocao.pl
+++ adocao.pl
@@ -1,75 +1,32 @@
 % (Instancia, Nome, Idade, Sexo, Nacionalidade, Raça)
 
-pessoa(caos, 'Caos', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(gaia, 'Gaia', 150, 'Feminino', 'Brasileiro', 'Pardo' ).
-pessoa(urano, 'Uranos', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(cronos, 'Cronos', 120, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(reia, 'Reia', 130, 'Feminino', 'Brasileiro', 'Pardo').
-pessoa(japeto, 'Japeto', 110, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(epimeteu, 'Epimeteu', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(prometeu, 'Prometeu', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(atlas, 'Atlas', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(hades, 'Hades', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(hera, 'Hera', 150, 'Feminino', 'Brasileiro', 'Pardo').
-pessoa(poseidon, 'Poseidon', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(demeter, 'Demeter', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(zeus, 'Zeus', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(dione, 'Dione', 150, 'Feminino', 'Brasileiro', 'Pardo').
-pessoa(semele, 'Semele', 150, 'Feminino', 'Brasileiro', 'Pardo').
-pessoa(afrodite, 'Afrodite', 150, 'Feminino', 'Brasileiro', 'Pardo').
-pessoa(dionisio, 'Dionisio', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(heracles, 'Heracles', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(ares, 'Ares', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(hefesto, 'Hefesto', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(persefone, 'Persefone', 150, 'Masculino', 'Brasileiro', 'Pardo').
-pessoa(atena, 'Atena', 150, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(caos, 		'Caos', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(gaia, 		'Gaia', 150, 'Feminino', 'Brasileiro', 'Pardo' ).
+pessoa(urano, 		'Uranos', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(cronos, 		'Cronos', 120, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(reia, 		'Reia', 130, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(japeto, 		'Japeto', 110, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(epimeteu, 	'Epimeteu', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(prometeu, 	'Prometeu', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(atlas, 		'Atlas', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(hades, 		'Hades', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(hera, 		'Hera', 150, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(poseidon, 	'Poseidon', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(demeter, 	'Demeter', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(zeus, 		'Zeus', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(dione, 		'Dione', 150, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(semele, 		'Semele', 150, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(afrodite, 	'Afrodite', 150, 'Feminino', 'Brasileiro', 'Pardo').
+pessoa(dionisio, 	'Dionisio', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(heracles, 	'Heracles', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(ares, 		'Ares', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(hefesto, 	'Hefesto', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(persefone, 	'Persefone', 150, 'Masculino', 'Brasileiro', 'Pardo').
+pessoa(atena, 		'Atena', 150, 'Feminino', 'Brasileiro', 'Pardo').
 
-/*
-nome(caos, 'Caos').
-nome(gaia, 'Gaia').
-nome(urano, 'Uranos').
-nome(cronos, 'Cronos').
-nome(reia, 'Reia').
-nome(japeto, 'Japeto').
-nome(epimeteu, 'Epimeteu').
-nome(prometeu, 'Prometeu').
-nome(atlas, 'Atlas').
-nome(hades, 'Hades').
-nome(hera, 'Hera').
-nome(poseidon, 'Poseidon').
-nome(demeter, 'Demeter').
-nome(zeus, 'Zeus').
-nome(dione, 'Dione').
-nome(semele, 'Semele').
-nome(afrodite, 'Afrodite').
-nome(dionisio, 'Dionisio').
-nome(heracles, 'Heracles').
-nome(ares, 'Ares').
-nome(hefesto, 'Hefesto').
-nome(persefone, 'Persefone').
-nome(atena, 'Atena').
+nome(X, Y) 				:- pessoa(X, Y, _, _, _, _).
+idade(X, Y) 			:- pessoa(X, _, Y, _, _, _).
+sexo(X, Y) 				:- pessoa(X, _, _, Y, _, _).
+nacionalidade(X, Y) 	:- pessoa(X, _, _, _, Y, _).
+raca(X, Y) 				:- pessoa(X, _, _, _, _, Y).
 
-
-idade(caos, ).
-idade(gaia).
-idade(urano).
-idade(cronos).
-idade(reia).
-idade(japeto).
-idade(epimeteu).
-idade(prometeu).
-idade(atlas).
-idade(hades).
-idade(hera).
-idade(poseidon).
-idade(demeter).
-idade(zeus).
-idade(dione).
-idade(semele).
-idade(afrodite).
-idade(dionisio).
-idade(heracles).
-idade(ares).
-idade(hefesto).
-idade(persefone).
-idade(atena).*/
